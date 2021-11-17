c1c6e05587e890ba30254a386ae778ba1233a60c
adybelli-commerce <gitlab@adybelli.com>
2021-11-17 16:41:36 +0500

    middleware fixed

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,13 +1,13 @@
 import { authMiddleware } from '@/middleware/auth'
 import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
 
   if (req.ua?.isBot) return null
   if (!req.page.name) return res
 
-  const authRes = authMiddleware({ req, res })
+  const authRes = await authMiddleware({ req, res })
   if (authRes) return authRes
 
   return res
